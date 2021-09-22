978e18fc1210d4a347d3ee1393dbffa59887d661
Pauline Marot <pauline.marot2@outlook.fr>
2021-09-22 14:56:50 +0200

    EchecPaiement

--- src/Controller/OrderCancelController.php
+++ src/Controller/OrderCancelController.php
@@ -2,14 +2,13 @@
 
 namespace App\Controller;
 
-use App\Classe\Cart;
 use App\Entity\Order;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class OrderSuccessController extends AbstractController
+class OrderCancelController extends AbstractController
 {
     private $entityManager;
 
@@ -18,8 +17,8 @@ class OrderSuccessController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    #[Route('/commande/merci/{stripeSessionId}', name: 'order_success')]
-    public function index(Cart $cart, $stripeSessionId): Response
+    #[Route('/commande/erreur/{stripeSessionId}', name: 'order_cancel')]
+    public function index($stripeSessionId): Response
     {
         $order = $this->entityManager->getRepository(Order::class)->findOneByStripeSessionId($stripeSessionId);
 
@@ -27,20 +26,8 @@ class OrderSuccessController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
-        if (!$order->getIsPaid()) {
-            // vider la session "Cart"
-            $cart->remove();
-
-            // modif le status isPaid de notre commande en mettant 1
-            $order->setIsPaid(1);
-            $this->entityManager->flush();
-
-            // envoyer un email à notre client pour lui confirmer sa commande
-
-        }
-
-        // afficher les quelques infos de la commande de l'utilisateur
-        return $this->render('order_success/index.html.twig', [
+        // envoyer un email à notre client pour lui indiquer l'échec de la commande
+        return $this->render('order_cancel/index.html.twig', [
             'order' => $order
         ]);
     }
