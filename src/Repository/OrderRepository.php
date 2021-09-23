9d00e94579eae036db268f9c5e4caf8ede6ace06
Pauline Marot <pauline.marot2@outlook.fr>
2021-09-23 10:39:47 +0200

    AfficherCommandesEspaceMembre

--- src/Repository/OrderRepository.php
+++ src/Repository/OrderRepository.php
@@ -19,6 +19,23 @@ class OrderRepository extends ServiceEntityRepository
         parent::__construct($registry, Order::class);
     }
 
+    // findSuccessOrders() permet d'afficher l'espace membre dans l'utilisateur
+    public function findSuccessOrders($user)
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.isPaid = 1')
+            ->andWhere('o.user = :user')
+            ->setParameter('user', $user)
+            ->orderBy('o.id', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+    }
+
+
+
+
+
     // /**
     //  * @return Order[] Returns an array of Order objects
     //  */
