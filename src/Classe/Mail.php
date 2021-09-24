2f6de11fdbbc1154887664e1e726ba85dfb46ada
Pauline Marot <pauline.marot2@outlook.fr>
2021-09-24 10:51:53 +0200

    AjoutMail

--- src/Classe/Mail.php
+++ src/Classe/Mail.php
@@ -2,8 +2,45 @@
 
 namespace App\Classe;
 
+use Mailjet\Client;
+use Mailjet\Resources;
+
 class Mail
 {
-    private $api_key = '0a6d94cc6f396f5248bb93d23ac2580f';
-    private  $api_key_secret = 'd26fa9a16aabda2af3fd5e3a7a4a70bf';
+    private string $api_key = '0a6d94cc6f396f5248bb93d23ac2580f';
+    private string $api_key_secret = 'd26fa9a16aabda2af3fd5e3a7a4a70bf';
+
+    public function send($to_email, $to_name, $subject, $content)
+    {
+        $mj = new Client($this->api_key, $this->api_key_secret, true, ['version' => 'v3.1']);
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "pauline.marot2@outlook.fr",
+                        'Name' => "La Boutique De Pauline"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $to_email,
+                            'Name' => $to_name
+                        ]
+                    ],
+                    'TemplateID' => 3195571,
+                    'TemplateLanguage' => true,
+                    'Subject' => $subject,
+                    'Variables' => [
+                        'content' => $content,
+                    ]
+                ]
+            ]
+        ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success();
+
+       // && dd($response->getData())
+    }
+
 }
+
+
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+      /* $mail = new Mail();
+        $mail->send('pauline.marot2@outlook.fr', 'Pauline Marot', 'Mon premier mail', "Bonjour Pauline, Comment vas tu ?");*/
+
         return $this->render('home/index.html.twig');
     }
 }
